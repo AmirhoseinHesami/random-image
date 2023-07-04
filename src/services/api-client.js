5b4edb9b397724d88504f50010196bb0924ec143
AmirhoseinHesami <amirhosein1383hesami@gmail.com>
2023-07-04 09:01:35 +0330

    [Fix Bug]☕

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.api-ninjas.com/v1",
   headers: {
-    "X-Api-Key": "HoICMbHVZha8gX313XHxmw==Zfwq9nyLzTw42iTY",
+    "X-Api-Key": "5H3FhIVZ7FoUwPqkBVik3KrdakbLhmm8oueSf2RO",
     Accept: "image/jpg",
   },
   responseType: "blob",
