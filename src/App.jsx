fd463d476edd9d8f988a6942d9ffab5639a0c1e5
AmirhoseinHesami <amirhosein1383hesami@gmail.com>
2023-07-04 03:57:23 +0330

    build initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
-import { Button } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Menu,
+  MenuList,
+  MenuItem,
+  MenuButton,
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Center,
+  AbsoluteCenter,
+} from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="red">Yes</Button>
+      <Box bg="tomato" w="100%" h="100vh" p={4} color="white">
+        <Flex justifyContent={"space-between"} bgColor={"white"}>
+          <Button>Reload</Button>
+          <Menu bgColor="balck">
+            <MenuButton as={Button}>Actions</MenuButton>
+            <MenuList color={"black"}>
+              <MenuItem>Download</MenuItem>
+              <MenuItem>Create a Copy</MenuItem>
+              <MenuItem>Mark as Draft</MenuItem>
+              <MenuItem>Delete</MenuItem>
+              <MenuItem>Attend a Workshop</MenuItem>
+            </MenuList>
+          </Menu>
+          <Button>Theme Mode</Button>
+        </Flex>
+        <Center>
+          <AbsoluteCenter>
+            <Card>
+              <CardBody>
+                <Image></Image>
+              </CardBody>
+              <CardFooter>
+                <Button>Watermark</Button>
+                <Button>Grayscale</Button>
+              </CardFooter>
+            </Card>
+          </AbsoluteCenter>
+        </Center>
+      </Box>
     </>
   );
 }
